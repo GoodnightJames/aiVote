34f98b068c3144db36a8183db4d8ce7671122085
GoodnightJames <128565926+GoodnightJames@users.noreply.github.com>
2023-06-03 01:21:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,21 @@
-// Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyC07q7g5RUt9IMAH6TfOcO-LnS1MLP0Iiw",
-    authDomain: "aivote-842d3.firebaseapp.com",
-    projectId: "aivote-842d3",
-    storageBucket: "aivote-842d3.appspot.com",
-    messagingSenderId: "1001056006585",
-    appId: "1:1001056006585:web:5208ea815e6f8e8bad0668",
-    measurementId: "G-P9FN6MY6ZE"
-};
+window.onload = function() {
+    const imagesContainer = document.getElementById('images-container');
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+    fetch('images.json')
+        .then(response => response.json())
+        .then(data => {
+            data.images.forEach(image => {
+                const img = document.createElement('img');
+                img.src = image.image_path;
+                imagesContainer.appendChild(img);
 
-// get images-container element
-let imagesContainer = document.getElementById('images-container');
-
-fetch('images.json')
-    .then(response => response.json())
-    .then(data => {
-        data.images.forEach(image => {
-            let imgElement = document.createElement('img');
-            imgElement.src = image.image_path;
-            imgElement.onclick = function() {
-                firebase.database().ref('votes/' + image.id).transaction(function(currentVote) {
-                    return (currentVote || 0) + 1;
+                img.addEventListener('click', function() {
+                    // Add a new vote entry in Firebase
+                    firebase.database().ref('votes').push({
+                        id: image.id,
+                        filename: image.filename
+                    });
                 });
-            };
-            imagesContainer.appendChild(imgElement);
+            });
         });
-    });
+}
