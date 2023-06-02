642ec0e432197b37d4fd21ea0ab8d6a36cc96750
GoodnightJames <128565926+GoodnightJames@users.noreply.github.com>
2023-06-03 01:24:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-window.onload = function() {
-    const imagesContainer = document.getElementById('images-container');
+const imagesContainer = document.getElementById('images-container');
 
-    fetch('images.json')
-        .then(response => response.json())
-        .then(data => {
-            data.images.forEach(image => {
-                const img = document.createElement('img');
-                img.src = image.image_path;
-                imagesContainer.appendChild(img);
+fetch('images.json')
+    .then(response => response.json())
+    .then(data => {
+        data.images.forEach(image => {
+            const img = document.createElement('img');
+            img.src = image.image_path;
+            imagesContainer.appendChild(img);
 
-                img.addEventListener('click', function() {
-                    // Add a new vote entry in Firebase
-                    firebase.database().ref('votes').push({
-                        id: image.id,
-                        filename: image.filename
-                    });
+            img.addEventListener('click', function() {
+                // Add a new vote entry in Firebase
+                firebase.database().ref('votes').push({
+                    id: image.id,
+                    filename: image.filename
                 });
             });
         });
-}
+    });
