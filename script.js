ff191baf1d2212dc8f24276d817ee27b4bcedcb0
GoodnightJames <128565926+GoodnightJames@users.noreply.github.com>
2023-06-03 02:02:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,40 @@
-// Initialize Firebase database
-const db = firebase.database();
+// Initialize Firebase
+var config = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  databaseURL: "YOUR_DATABASE_URL",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID"
+};
+firebase.initializeApp(config);
 
-fetch('images.json')
-    .then(response => response.json())
-    .then(data => displayImages(data.images));
+var database = firebase.database();
 
 function displayImages(images) {
-    const imagesContainer = document.getElementById('images-container');
-
-    images.forEach(image => {
-        const imgElement = document.createElement('img');
-        imgElement.src = image.image_path;
-        imgElement.alt = image.filename;
-        imgElement.addEventListener('click', () => voteForImage(image.id));
-
-        imagesContainer.appendChild(imgElement);
-    });
-}
-
-function voteForImage(imageId) {
-    const imageRef = db.ref('images/' + imageId);
-    imageRef.transaction(currentVotes => {
-        if (currentVotes === null) {
-            return 1;
-        } else {
-            return currentVotes + 1;
+  var imageContainer = document.getElementById("imageContainer");
+  imageContainer.innerHTML = ""; // Clear the container
+  for (var key in images) {
+    var img = document.createElement("img");
+    img.src = images[key].url;
+    img.onclick = function() {
+      database.ref("images/" + key).transaction(function(image) {
+        if (image) {
+          image.votes++;
         }
-    });
+        return image;
+      });
+      fetchImages(); // Refresh the images after a vote
+    };
+    imageContainer.appendChild(img);
+  }
 }
+
+function fetchImages() {
+  // Fetch the four images with the least number of votes
+  database.ref("images").orderByChild("votes").limitToFirst(4).once("value", function(snapshot) {
+    displayImages(snapshot.val());
+  });
+}
+
+fetchImages(); // Fetch the images when the page loads
