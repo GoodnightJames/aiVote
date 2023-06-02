1a91e61a43f4fd64d04c6e48b728ca68b3838ca3
GoodnightJames <128565926+GoodnightJames@users.noreply.github.com>
2023-06-03 01:17:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,82 +1,31 @@
-var images = [];
-var currentIndex = 0;
-var currentRound = 1;
+// Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyC07q7g5RUt9IMAH6TfOcO-LnS1MLP0Iiw",
+    authDomain: "aivote-842d3.firebaseapp.com",
+    projectId: "aivote-842d3",
+    storageBucket: "aivote-842d3.appspot.com",
+    messagingSenderId: "1001056006585",
+    appId: "1:1001056006585:web:5208ea815e6f8e8bad0668",
+    measurementId: "G-P9FN6MY6ZE"
+};
 
-// Firebase Database reference
-var database = firebase.database();
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-function getImages() {
-    fetch('images.json')
-        .then(response => response.json())
-        .then(data => {
-            images = data;
-            console.log('Images fetched:', images);
-            displayImages();
-        })
-        .catch(error => console.error(error));
-}
+// get images-container element
+let imagesContainer = document.getElementById('images-container');
 
-function displayImages() {
-    const imageContainer = document.getElementById('image-container');
-    imageContainer.innerHTML = '';
-
-    if (currentIndex < images.length) {
-        const imageWrapper = document.createElement('div');
-        imageWrapper.classList.add('image-wrapper');
-
-        for (let i = currentIndex; i < currentIndex + 4; i++) {
-            if (i < images.length) {
-                const img = document.createElement('img');
-                img.src = images[i].image_path;
-                img.alt = 'Image';
-                console.log('Image ID being added to image:', images[i].id);
-                img.addEventListener('click', () => voteImage(images[i].id));
-                imageWrapper.appendChild(img);
-            }
-        }
-
-        imageContainer.appendChild(imageWrapper);
-    } else {
-        const noImagesMessage = document.createElement('p');
-        noImagesMessage.textContent = 'No more images available.';
-        imageContainer.appendChild(noImagesMessage);
-    }
-}
-
-function voteImage(imageId) {
-    console.log('Vote counted for imageId:', imageId);
-
-    // Record vote in Firebase
-    var votesRef = database.ref('votes/' + imageId);
-    votesRef.transaction(function(currentVotes) {
-        // If votes has never been set, currentVotes will be `null`.
-        return (currentVotes || 0) + 1;
+fetch('images.json')
+    .then(response => response.json())
+    .then(data => {
+        data.images.forEach(image => {
+            let imgElement = document.createElement('img');
+            imgElement.src = image.image_path;
+            imgElement.onclick = function() {
+                firebase.database().ref('votes/' + image.id).transaction(function(currentVote) {
+                    return (currentVote || 0) + 1;
+                });
+            };
+            imagesContainer.appendChild(imgElement);
+        });
     });
-
-    currentIndex += 4;
-
-    if (currentIndex >= images.length) {
-        console.log('Round ' + currentRound + ' complete.');
-
-        if (currentRound === 1) {
-            console.log('Final round reached.');
-            console.log('Remaining images:', images);
-            return;
-        }
-
-        currentRound++;
-        currentIndex = 0;
-        shuffleImages();
-    }
-
-    displayImages();
-}
-
-function shuffleImages() {
-    for (let i = images.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [images[i], images[j]] = [images[j], images[i]];
-    }
-}
-
-getImages();
